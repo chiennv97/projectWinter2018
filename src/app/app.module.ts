26c435303aeb6656134c7c6f2579ffb399c0776e
chiennv <chiennv97@gmail.com>
2018-11-10 08:42:32 +0700

    version before change login firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { rootRouterConfig } from './app.routes';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
+import {RegisterComponent} from './register/register.component';
+import {AuthService} from './core/auth.service';
+import {UserService} from './core/user.service';
+import {UserResolver} from './user/user.resolver';
+import {AuthGuard} from './core/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
     DragDropModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
-  providers: [],
+  providers: [AuthService, UserService, UserResolver, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBE8-oCjEkK4pbi2WLa_tJoKnEcICvzDLM',
+    authDomain: 'project-winter-2018.firebaseapp.com',
+    databaseURL: 'https://project-winter-2018.firebaseio.com',
+    projectId: 'project-winter-2018',
+    storageBucket: 'project-winter-2018.appspot.com',
+    messagingSenderId: '736582718233'
+  }
 };
 
 /*
